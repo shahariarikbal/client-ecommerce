edb3a01a0b5f9b6edb4adad3cbdbec98b7a768b0
Ahmed Sabbir <theahmedsabbir@gmail.com>
2022-03-03 21:53:52 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//=============== Basic Routes ====================//
+Route::get('clear', function () {
+    \Artisan::call('cache:forget spatie.permission.cache');
+    \Artisan::call('view:clear');
+    \Artisan::call('cache:clear');
+    \Artisan::call('config:clear');
+    \Artisan::call('route:clear');
+    dd("All clear!");
+});
+
+
 
 Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
 
